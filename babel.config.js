d2b21ca5c4fc684a8cb65c19b3a59845673ab18c
phu phan <>
2023-09-11 00:33:08 +0700

    add path alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@navigation': './src/navigation',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@assets': './src/assets',
+          '@scenes': './src/scenes',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
